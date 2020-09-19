e9e5a6b7d936e783e95c151bc3f27ac944a36320
Filip Mularski <mularskif@gmail.com>
2020-09-19 12:36:27 +0200

    creating model, checking its accuracy, predicting label

--- linear_regression_students_example.py
+++ linear_regression_students_example.py
@@ -21,3 +21,20 @@ Y = np.array(data[predict])  # array with label
 # cannot set 100% as test because the algorithm will know all the answers before testing
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size=0.1)
 
+linear = linear_model.LinearRegression()    # the model
+linear.fit(x_train, y_train)    # fitting the line for train set
+accuracy = linear.score(x_test, y_test)     # score() returns the accuracy of the model based on running test data
+
+print('Coefficient:\n', linear.coef_)       # coefficient is a in y = ax + b, one for each attribute
+# 5 attributes so 5-dim space, the bigger coefficient, the greater influence the given attribute has on the result
+print('Intercept:\n', linear.intercept_)    # intercept is b in y = ax + b
+
+# PREDICTING STUDENT'S GRADE
+
+y_predictions = linear.predict(x_test)      # predicts label based on testing set
+for i in range(len(y_predictions)):         # iterating over all predictions
+    print(y_predictions[i], x_test[i], y_test[i])   # displaying predictions, attributes and actual label value
+
+
+
+
