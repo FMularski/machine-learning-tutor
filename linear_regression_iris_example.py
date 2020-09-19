dd2ec4056ce8bb1aba99b0176b592342416f1435
Filip Mularski <mularskif@gmail.com>
2020-09-19 19:34:35 +0200

    saving/loading models with pickle

--- linear_regression_iris_example.py
+++ linear_regression_iris_example.py
@@ -3,6 +3,8 @@ import numpy as np
 import sklearn
 from sklearn import linear_model
 from sklearn.preprocessing import LabelEncoder
+import pickle
+
 
 # reading the data, giving names to each column
 raw_data = pd.read_csv('data/iris/iris.data', names=['sepal_length', 'sepal_width', 'petal_length', 'petal_width',
@@ -20,13 +22,25 @@ Y = np.array([data[predict]])
 
 Y = Y.reshape((150, 1))     # reshaping Y array as X.shape[0] and Y.shape[0] must be matching
 
-# dividing X and Y into training and testing sets
-x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size=0.1)
+top_accuracy = 0
+while top_accuracy < 0.95:  # create models until its accuracy is higher than 95%
+    # dividing X and Y into training and testing sets
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size=0.1)
 
-linear = linear_model.LinearRegression()    # creating linear model
-linear.fit(x_train, y_train)    # finding the line for train values (training model)
+    linear = linear_model.LinearRegression()    # creating linear model
+    linear.fit(x_train, y_train)    # finding the line for train values (training model)
 
-print('Model accuracy:', linear.score(x_test, y_test))  # checking its accuracy by using test set
+    accuracy = linear.score(x_test, y_test)
+    print('Model accuracy:', accuracy)  # checking its accuracy by using test set
+
+    if accuracy > top_accuracy:
+        top_accuracy = accuracy
+        with open('saved_models/student_model.pickle', 'wb') as f:  # saving model to a file by using pickle
+            pickle.dump(linear, f)
+
+with open('saved_models/student_model.pickle', 'rb') as f:  # loading model from a file by using pickle
+    linear = pickle.load(f)
+
 
 y_predictions = linear.predict(x_test)    # predicting label by using test set
 
@@ -35,6 +49,3 @@ for i, y_prediction in enumerate(y_predictions):
     print('Label:', y_test[i][0])
     print('Attributes:\n', x_test[i])
     print('*' * 20)
-
-
-
